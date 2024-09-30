9f232396214276c55137a3689a9a6f90b9234c9a
kamakshi515 <gedalakamakshi2004@gmail.com>
2024-09-30 11:43:01 +0530

    click commit changes
    hello git hub

---
+++ mainexception.java
@@ -0,0 +1,13 @@
+public class Mainexception{
+static void checkage(int age){
+ if(age<18){
+throw new Arithmeticexception("not granted")
+}else{
+system.out.println("acess granted":,you are old enough)
+}
+}
+public static void main(string[] args)
+system.out.println(checkage(15));
+}
+}
+}
